a7adacfafc47748053f01c342a1db668558f0bb3
ihsantacer <enisyaren58@gmail.com>
2022-12-31 23:52:24 +0300

    V1

---
+++ A01.java
@@ -0,0 +1,6 @@
+public class A01 {
+    public static void main(String[] args) {
+        int sayı=1;
+        int sayı1=0;
+    }
+}
---
+++ A03.java
@@ -0,0 +1,9 @@
+public class A03 {
+
+    public static void main(String[] args) {
+     int sayı=10;
+     int sayı1=0;
+
+
+    }
+}
